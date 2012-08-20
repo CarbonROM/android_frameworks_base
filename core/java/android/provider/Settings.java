435d74480f414340b959167b6fa80e9d74ca1dac
Konsta <konsta09@gmail.com>
2012-08-20 10:39:57 +0300

    Framework: Volume key cursor control
    This feature is moved to framework so it also works with third
    party keyboards.
    
    Change-Id: I8e20240e7bee5351ab20bb3d701eb95a5fd3e112

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4463,6 +4463,15 @@ public final class Settings {
         public static final String ANIMATOR_DURATION_SCALE = Global.ANIMATOR_DURATION_SCALE;
 
         /**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
          * Control whether the accelerometer will be used to change screen
          * orientation.  If 0, it will not be used unless explicitly requested
          * by the application; if 1, it will be used by default unless explicitly
