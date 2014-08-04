45113afb5511d1a41dc56ce091ef9bec309b1920
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    fwb: Add option to scramble pin layout when unlocking [1/2]
    Adapted to Lineage SDK and squashed with the following changes:
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 10:40:41 -0700
    
          Keyguard: Don't disable visibility when scrambling pin.
    
            Otherwise the first numbers text is flipped to invisible
            even though it shouldn't be.
    
          Change-Id: Ia2b80d594e7f11af7a60a85bdb4ea9909d0ac20b
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 16:21:27 -0700
    
          Keyguard: Fix scramblepin logic.
    
          Change-Id: I71e6d26e853fa7bfaeea4b3256902881ac7f74fc
    
    Change-Id: Ibe8611eb0b8a5bd9275bc96682d2cbafaa37d8f5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4587,6 +4587,12 @@ public final class Settings {
         public static final String QUICK_SETTINGS_REBOOT_OPTIONS = "quick_settings_reboot_options";
 
         /**
+         * Whether to scramble a pin unlock layout
+         * @hide
+         */
+        public static final String LOCKSCREEN_PIN_SCRAMBLE_LAYOUT = "lockscreen_scramble_pin_layout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
