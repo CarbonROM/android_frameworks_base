f56fdef8e90fecd22bfcf9b90c7122ba35871ee9
Lars Greiss <kufikugel@googlemail.com>
2014-12-08 22:48:12 +0100

    Frameworks: inital bringup of slim actions and helper classes
    Bring back slimactions.
    
    - Most actions are available and adapted to LP (mainly statusbar actions
    we did on kk need to be ported back again)
    - Cleanup of old code
    - Renamed classes and methods
    
    and more.....
    
    This is the base for all the comming features now
    
    Change-Id: I0b4458325e248319217b8e41b64e2cf1e45f56cd

--- core/java/com/android/internal/util/slim/ActionConstants.java
+++ core/java/com/android/internal/util/slim/ActionConstants.java
@@ -28,7 +28,6 @@ public class ActionConstants {
     public static final String ACTION_MENU_BIG             = "**menu_big**";
     public static final String ACTION_POWER                = "**power**";
     public static final String ACTION_NOTIFICATIONS        = "**notifications**";
-    public static final String ACTION_SETTINGS_PANEL       = "**settings_panel**";
     public static final String ACTION_RECENTS              = "**recents**";
     public static final String ACTION_SCREENSHOT           = "**screenshot**";
     public static final String ACTION_IME                  = "**ime**";
@@ -54,7 +53,6 @@ public class ActionConstants {
     public static final String ACTION_MEDIA_NEXT           = "**media_next**";
     public static final String ACTION_MEDIA_PLAY_PAUSE     = "**media_play_pause**";
     public static final String ACTION_WAKE_DEVICE          = "**wake_device**";
-    public static final String ACTION_RESTARTUI          = "**restartui**";
 
     // no action
     public static final String ACTION_NULL            = "**null**";
@@ -99,12 +97,6 @@ public class ActionConstants {
         + ICON_EMPTY           + ACTION_DELIMITER
         + ACTION_IME           + ACTION_DELIMITER
         + ACTION_NULL          + ACTION_DELIMITER
-        + ICON_EMPTY;
-
-    public static final String QUICK_TILE_CONFIG_DEFAULT =
-          ACTION_SCREENSHOT + ACTION_DELIMITER
-        + ICON_EMPTY        + ACTION_DELIMITER
-        + ACTION_POWER_MENU + ACTION_DELIMITER
         + ICON_EMPTY;
 
 }
