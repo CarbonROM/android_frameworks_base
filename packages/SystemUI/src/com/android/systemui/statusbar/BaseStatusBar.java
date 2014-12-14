b6a17164b460032e18b6203bb4d426be23a1a56b
Lars Greiss <kufikugel@googlemail.com>
2014-12-14 15:35:25 +0100

    Frameworks: Only set package name on user heads ups
    Let us not influence the default aosp behaviour which could conflict if an app
    is strange coded
    
    Change-Id: If69e40ce932ce7e77886d250879346a9a43920a2

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2355,7 +2355,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         if (isHeadsUpInSnooze()) {
             return false;
         }
-        mHeadsUpPackageName = sbn.getPackageName();
         // some predicates to make the boolean logic legible
         boolean isNoisy = (notification.defaults & Notification.DEFAULT_SOUND) != 0
                 || (notification.defaults & Notification.DEFAULT_VIBRATE) != 0
@@ -2395,6 +2394,10 @@ public abstract class BaseStatusBar extends SystemUI implements
                     && !accessibilityForcesLaunch
                     && !isExpanded
                     && !keyguardIsShowing;
+
+            if (interrupt) {
+                mHeadsUpPackageName = sbn.getPackageName();
+            }
         }
 
         try {
