9b05eb426fa277ff60d8906730fbd0ef01b56141
nadlabak <pavel@doshaska.net>
2014-12-20 23:56:55 +0200

    Forward port 'Swap volume buttons' (1/3)
    [mikeioannina]: Adjust for 5.0 changes
    
    Change-Id: I1b45da63b815aa1b3ddf7cda2b7afb0872ab433f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5009,6 +5009,12 @@ public final class Settings {
         public static final String ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
 
         /**
+        * Swap volume buttons on rotation
+        * @hide
+        */
+        public static final String SWAP_VOLUME_KEYS_ON_ROTATION = "swap_volume_keys_on_rotation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
