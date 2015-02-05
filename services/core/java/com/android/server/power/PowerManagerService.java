2abd5807605ec2a51660861e2d0dbc1db1461ba0
dankoman <dankoman30@gmail.com>
2015-02-05 15:12:13 +0100

    [1/2] Frameworks: let InCallUI handle proximity sensor for incoming calls
    fixes "proximity sensor doesn't control screen state on incoming
            call.  must manually turn on screen to see who's calling."
    
    Change-Id: I738b3bbd328992c8a8010a0c8d9aa1e444cfa9f9

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -198,6 +198,7 @@ public final class PowerManagerService extends SystemService
     private int mButtonBrightnessSettingDefault;
     private int mKeyboardBrightness;
     private int mKeyboardBrightnessSettingDefault;
+    private TelephonyManager mTelephonyManager;
 
     private final Object mLock = new Object();
 
@@ -3317,7 +3318,8 @@ public final class PowerManagerService extends SystemService
 
             TelephonyManager tm = (TelephonyManager)mContext.getSystemService(
                     Context.TELEPHONY_SERVICE);
-            boolean hasIncomingCall = tm.getCallState() == TelephonyManager.CALL_STATE_RINGING;
+            boolean hasIncomingCall = (getTelephonyManager().getCallState()
+                    == TelephonyManager.CALL_STATE_RINGING);
 
             if (mProximityWakeSupported && mProximityWakeEnabled && mProximitySensor != null
                     && !hasIncomingCall) {
@@ -3328,6 +3330,14 @@ public final class PowerManagerService extends SystemService
             } else {
                 r.run();
             }
+        }
+
+        TelephonyManager getTelephonyManager() {
+            if (mTelephonyManager == null) {
+                mTelephonyManager = (TelephonyManager)mContext.getSystemService(
+                        Context.TELEPHONY_SERVICE);
+            }
+            return mTelephonyManager;
         }
 
         private void runPostProximityCheck(final Runnable r) {
