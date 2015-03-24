23bced6f55a4b05639234ead44ad01a62aa85b75
BigBrother1984 <carlosavignano@aospa.co>
2015-03-24 22:31:30 +0200

    FW_Base - [Heads up] Add touch outside event
    - Codes to it was picked from here:
    https://github.com/AOSPA/android_frameworks_base/commit/b62eeedf6f99760f88ee62d6c163c92092f4294f
    
    All the thanks goes to @BigBrother1984
    
    Change-Id: I772557b199c5c34baebe61e2346797bfb6162252
    
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
@@ -139,6 +139,11 @@ public class TvStatusBar extends BaseStatusBar {
         return true;
     }
 
+    @Override
+    public boolean isExpandedVisible() {
+        return false;
+    }
+
     public View getStatusBarView() {
         return null;
     }
