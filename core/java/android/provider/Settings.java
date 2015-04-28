b9383dbc5dd8055082793a4d3042fbe30888515f
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-04-28 20:42:00 +0100

    Base: Add a controller for in-call proximity sensor (1/3)
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: I7c56093e624a680e53839d68cc6293f53614dc33

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4892,6 +4892,12 @@ public final class Settings {
         public static final String RECENT_CARD_TEXT_COLOR = "recent_card_text_color";
 
         /**
+         * Whether to use the proximity sensor to turn the screen on/off during a call
+         * @hide
+         */
+        public static final String IN_CALL_PROXIMITY_SENSOR = "in_call_proximity_sensor";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
