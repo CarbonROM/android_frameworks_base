684603ae74115fa7fe8a3cf85a2a480e2520f626
STELIX <ssspinni@gmail.com>
2015-05-01 21:27:39 +0200

    FW_Base - SliPIE & SlimActions: Fix the build & missings
    Change-Id: I1114233034f1b853d52057d89955ed63c5d60ce6
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -153,11 +153,10 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected static final int MSG_HIDE_HEADS_UP = 1029;
     protected static final int MSG_ESCALATE_HEADS_UP = 1030;
     protected static final int MSG_DECAY_HEADS_UP = 1031;
-    protected static final int MSG_TOGGLE_LAST_APP = 1032;
-    protected static final int MSG_TOGGLE_KILL_APP = 1033;
-    protected static final int MSG_TOGGLE_SCREENSHOT = 1034;
-
-    protected static final int MSG_SET_PIE_TRIGGER_MASK = 1033;
+    protected static final int MSG_SET_PIE_TRIGGER_MASK = 1032;
+    protected static final int MSG_TOGGLE_LAST_APP = 1033;
+    protected static final int MSG_TOGGLE_KILL_APP = 1034;
+    protected static final int MSG_TOGGLE_SCREENSHOT = 1035;
 
     protected static final boolean ENABLE_HEADS_UP = true;
     // scores above this threshold should be displayed in heads up mode.
@@ -1424,6 +1423,9 @@ public abstract class BaseStatusBar extends SystemUI implements
                      mSearchPanelView.show(false, true);
                  }
                  break;
+             case MSG_SET_PIE_TRIGGER_MASK:
+                 updatePieTriggerMask(m.arg1, m.arg2 != 0);
+                 break;
              case MSG_TOGGLE_LAST_APP:
                  if (DEBUG) Slog.d(TAG, "toggle last app");
                      getLastApp();
@@ -1435,9 +1437,6 @@ public abstract class BaseStatusBar extends SystemUI implements
              case MSG_TOGGLE_SCREENSHOT:
                  if (DEBUG) Slog.d(TAG, "toggle screenshot");
                  takeScreenshot();
-                 break;
-             case MSG_SET_PIE_TRIGGER_MASK:
-                 updatePieTriggerMask(m.arg1, m.arg2 != 0);
                  break;
             }
         }
