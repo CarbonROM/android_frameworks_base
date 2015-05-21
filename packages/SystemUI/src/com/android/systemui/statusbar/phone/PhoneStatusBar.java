079164bed2df08bb570efc067a8a26b48a26ca06
Alex Cruz <mazdarider23@gmail.com>
2015-05-21 01:17:14 +0000

    CAF Task Manager switch [1/2]
    Change-Id: I4b7c3041bfa74bcd7e2981a39a34860422f596fc
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1256,7 +1256,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
 	// task manager
-        if (mContext.getResources().getBoolean(R.bool.config_showTaskManagerSwitcher)) {
+	    if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.ENABLE_TASK_MANAGER, 0) == 1) {
             mTaskManagerPanel =
                     (LinearLayout) mStatusBarWindow.findViewById(R.id.task_manager_panel);
             mTaskManager = new TaskManager(mContext, mTaskManagerPanel);
@@ -3239,7 +3240,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mWaitingForKeyguardExit = false;
         recomputeDisableFlags(!force /* animate */);
         setInteracting(StatusBarManager.WINDOW_STATUS_BAR, true);
-	if (mContext.getResources().getBoolean(R.bool.config_showTaskManagerSwitcher)) {
+	if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.ENABLE_TASK_MANAGER, 0) == 1) {
             mTaskManager.refreshTaskManagerView();
         }
     }
