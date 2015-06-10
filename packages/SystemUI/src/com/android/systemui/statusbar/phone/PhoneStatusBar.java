9e2726bc6cc9fd3dc4fc1002939eb24a25e99b2b
Roman Birg <roman@cyngn.com>
2015-06-09 18:13:07 -0700

    SystemUI: always log statusbar state
    2231c6e1adefdb7636d9fe38a388f1b0660d30ea added more logging to show the
    differnt keyguard states, as well as the current statusbar state.
    However, it no longer logs the proper statusbar state because the call
    to log it was removed from setBarState(). Add the logging call back
    after setting the state.
    
    Change-Id: I517351fe8c5059bce8b615aafc0d14261d297854
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -5163,6 +5163,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         mState = state;
         mStatusBarWindowManager.setStatusBarState(state);
+        logStateToEventlog();
     }
 
     @Override
