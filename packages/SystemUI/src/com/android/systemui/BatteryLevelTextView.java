9f42d4c0a2a4ef4753a17a4e436d254b9cde0c82
tobitege <tobiasteschner@googlemail.com>
2015-06-11 18:39:50 +0200

    FW_Base - Fix default battery text color (2/2)
    On clean install the code used black as the default text color.
    This caused an unreadable percentage text when displayed
    inside the circle.
    
    Change-Id: I6485f7dbacf6fe145043a96a59a745e7abaf1daa

--- packages/SystemUI/src/com/android/systemui/BatteryLevelTextView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryLevelTextView.java
@@ -58,7 +58,6 @@ public class BatteryLevelTextView extends TextView implements
     private int mNewColor;
     private int mOldColor;
     private Animator mColorTransitionAnimator;
-
     private ContentResolver mResolver;
 
     private ContentObserver mObserver = new ContentObserver(new Handler()) {
@@ -73,7 +72,8 @@ public class BatteryLevelTextView extends TextView implements
         mRequestedVisibility = getVisibility();
 
         mNewColor = Settings.System.getInt(mResolver,
-                Settings.System.STATUSBAR_CLOCK_COLOR, 0xffffffff);
+            Settings.System.STATUS_BAR_BATTERY_STATUS_TEXT_COLOR,
+            0xffffffff);
         mOldColor = mNewColor;
         mColorTransitionAnimator = createColorTransitionAnimator(0, 1);
 
@@ -158,14 +158,13 @@ public class BatteryLevelTextView extends TextView implements
     }
 
     public void setTextColor(boolean isHeader) {
-        int headerColor = Settings.System.getInt(mResolver,
-                Settings.System.STATUS_BAR_EXPANDED_HEADER_TEXT_COLOR, 0xffffffff);
-        mNewColor = Settings.System.getInt(mResolver,
-                Settings.System.STATUS_BAR_BATTERY_STATUS_TEXT_COLOR, 0xff000000);
-
         if (isHeader) {
+            int headerColor = Settings.System.getInt(mResolver,
+                    Settings.System.STATUS_BAR_EXPANDED_HEADER_TEXT_COLOR, 0xffffffff);
             setTextColor(headerColor);
         } else {
+            mNewColor = Settings.System.getInt(mResolver,
+                    Settings.System.STATUS_BAR_BATTERY_STATUS_TEXT_COLOR, 0xffffffff);
             if (!mBatteryCharging && mBatteryLevel > 16) {
                 if (mOldColor != mNewColor) {
                     mColorTransitionAnimator.start();
