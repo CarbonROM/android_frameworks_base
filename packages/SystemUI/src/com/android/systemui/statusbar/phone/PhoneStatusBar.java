47e9576a1498140ff0db3d7134d41f1082f5016b
Roman Birg <roman@cyngn.com>
2015-06-26 01:48:38 -0700

    SystemUI: be more event driven in keyguard visualizer
    Change-Id: I1dc5ca44a3cff2d7faef679b4cc3cfdc32a1ab8c
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -5546,7 +5546,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         return !mNotificationData.getActiveNotifications().isEmpty();
     }
 
-    public void requestVisualizer(boolean show, int delay) {
+    public void requestVisualizer(Boolean show, int delay) {
         mBackdrop.requestVisualizer(show, delay);
     }
 
