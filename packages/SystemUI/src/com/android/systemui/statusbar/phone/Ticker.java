66423a6126b551e46b0021e532593e8c6b835d8b
STELIX <ssspinni@gmail.com>
2015-06-30 21:13:48 +0200

    Revert "SystemUI: vector support for navbar/pie icons"
    This reverts commit fb421971446e6db9ea480e8bb277234a66b83e7c.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
@@ -245,8 +245,8 @@ public abstract class Ticker {
                                Mode.MULTIPLY);
                         mIconSwitcher.setImageDrawable(seg.icon);
                     } else {
-                        mIconSwitcher.setImageDrawable(ImageHelper
-                                .getColoredDrawable(seg.icon, iconColor));
+                        mIconSwitcher.setImageBitmap(ImageHelper
+                                .getColoredBitmap(seg.icon, iconColor));
                     }
                 } else {
                     mIconSwitcher.setImageDrawable(seg.icon);
