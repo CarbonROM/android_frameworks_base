e1093c2a97b362afcdfd967261964237cfdfc179
STELIX <ssspinni@gmail.com>
2015-07-05 11:16:39 +0200

    Fw_Base - Lost in space
    Change-Id: Iea339937f6ec1e1abd7b73dbe50d574df5f756d8
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4674,6 +4674,12 @@ public final class Settings {
         public static final String MENU_VISIBILITY = "menu_visibility";
 
         /**
+         * Option to launch slim actions in floating mode
+         * @hide
+         */
+        public static final String SLIM_ACTION_FLOATS = "slim_action_floats";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
