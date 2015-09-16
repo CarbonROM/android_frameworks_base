7d3d580c336e9d6bca6792e6af5d1d725a9b2cb5
Chirayu Desai <cdesai@cyanogenmod.org>
2015-09-16 18:11:12 +0300

    Allow disabling the privacy guard notification (2/2)
    Original commit is made by chirayudesai
    
    Change-Id: I72e71da453a66a48c3ccfb1efb151f1939314612

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7311,6 +7311,12 @@ public final class Settings {
                 = "status_bar_locked_on_secure_keyguard";
 
         /**
+         * Whether a notification should be shown if privacy guard is enabled
+         * @hide
+         */
+        public static final String PRIVACY_GUARD_NOTIFICATION = "privacy_guard_notification";
+
+        /**
          * Default theme to use.  If empty, use holo.
          * @hide
          */
@@ -7574,6 +7580,7 @@ public final class Settings {
             PRIVACY_GUARD_DEFAULT,
             ADVANCED_REBOOT,
             DEVELOPMENT_SHORTCUT,
+            PRIVACY_GUARD_NOTIFICATION,
             STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD
         };
 
