f0355087d172b7488152241530bd2e5dec86b420
beanstown106 <nbenis106@gmail.com>
2015-10-23 21:07:35 -0400

    SystemUI: Allow disabling lock screen media art [1/2]
    * toggle to choose whether or not to show album art/media art on lockscreen
    Special thanks to atl4tis from Cataclysm rom for pointing out where to hook into this
    
    Change-Id: Id503c7c03098094155eb3a2e2276700d623bbad7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4324,6 +4324,12 @@ public final class Settings {
         public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 
         /**
+         * Whether to show media art on lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
