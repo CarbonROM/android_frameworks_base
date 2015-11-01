29acc1d458fadd6a7fe309958f13c4b07e5ef844
Carlo Savignano <carlosavignano@aospa.co>
2015-11-01 22:31:30 +0800

    input: support dynamic navigation bar toggling
    * Add system setting to dynamically switch navigation bar on/off.
    * Addresses NOUGAT-x
    
    Change-Id: I55da79fe8da83cf839cc91c1257ea91d2d632a40
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    
    input: introduce new navigation bar key event source
    
    * When generating virtual navigation bar KeyEvent, set
      a specific source fort it. There is no precise way to
      recognize if the KeyEvent is coming from KeyButtonView.
      Another way is passing another flag.
    
    Change-Id: I243c9f91afcbf4c430e2d526e8c814a5a7975e51
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    
    input: introduce custom key event policy
    
    * Ability to personalize double tap and long press behaviors.
      Those settings are added to the PRIVATE_SETTINGS list.
      Default AOSP behavior and original configs are kept intact.
      New overlay configs are added to separate pa_config.xml.
    * Define also a default behavior for supported key codes.
    * Add safe policy for custom handled inputs.
    * Don't allow supported key event codes when screen is non
      interactive til specific custom policy is needed.
    * Don't allow double tap and long press on keyguard.
    * Don't consume menu and recents key codes on keyguard.
    * Currently supported key codes:
      - KeyEvent.KEYCODE_HOME
      - KeyEvent.KEYCODE_BACK
      - KeyEvent.KEYCODE_MENU
      - KeyEvent.KEYCODE_ASSIST
      - KeyEvent.KEYCODE_APP_SWITCH
      - KeyEvent.KEYCODE_CAMERA
    * Available long press and double tap custom behaviors:
      0  = KEY_ACTION_NOTHING
      1  = KEY_ACTION_MENU
      2  = KEY_ACTION_APP_SWITCH
      3  = KEY_ACTION_SEARCH
      4  = KEY_ACTION_VOICE_SEARCH
      5  = KEY_ACTION_IN_APP_SEARCH
      6  = KEY_ACTION_CAMERA
      7  = KEY_ACTION_LAST_APP
    * If custom double tap or long press behavior is not defined,
      we will still intercept supported key events and handle them
      within default behaviors BUT inside our custom hooks.
    
    TODO> Check ADB input events working normally.
    TODO> Modify navbar to be able to use this policy.
    TODO> Unlock screen pinning with long back press,
          and set PIN/password required to unpin to on by default.
    
    Ticket: NOUGAT-2
    
    Change-Id: I77b32f16ea7672be5e9138a18d95607e100d2d37
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -227,6 +227,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     public static final boolean DEBUG_WINDOW_STATE = false;
 
+    // Reset navigation bar visibility after adding its view to window manager.
+    public static final boolean RESET_SYSTEMUI_VISIBILITY_FOR_NAVBAR = true;
+
     // additional instrumentation for testing purposes; intended to be left on during development
     public static final boolean CHATTY = DEBUG;
 
@@ -513,6 +516,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
         }
     }
+    private boolean mNavigationBarViewAttached;
 
     // ensure quick settings is disabled until the current user makes it through the setup wizard
     private boolean mUserSetup = false;
@@ -560,6 +564,24 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 if (!mUseHeadsUp) {
                     Log.d(TAG, "dismissing any existing heads up notification on disable event");
                     mHeadsUpManager.releaseAllImmediately();
+                }
+            }
+        }
+    };
+
+
+    final private ContentObserver mNavBarObserver = new ContentObserver(mHandler) {
+        @Override
+        public void onChange(boolean selfChange) {
+            boolean wasUsing = mUseNavBar;
+            mUseNavBar = Settings.System.getIntForUser(
+                    mContext.getContentResolver(), Settings.System.NAVIGATION_BAR_ENABLED, 0,
+                    UserHandle.USER_CURRENT) != 0;
+            Log.d(TAG, "navbar is " + (mUseNavBar ? "enabled" : "disabled"));
+            if (wasUsing != mUseNavBar) {
+                setNavBarEnabled(mUseNavBar);
+                if (mAssistManager != null) {
+                    mAssistManager.onConfigurationChanged();
                 }
             }
         }
@@ -798,7 +820,16 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // TODO: use MediaSessionManager.SessionListener to hook us up to future updates
         // in session state
 
-        addNavigationBar();
+
+        try {
+            boolean needsNavBar = mWindowManagerService.needsNavigationBar();
+            if (DEBUG) Log.v(TAG, "needsNavigationBar=" + needsNavBar);
+            if (needsNavBar) {
+                addNavigationBar();
+            }
+        } catch (RemoteException ex) {
+            // no window manager? good luck with that
+        }
 
         if (mSettingsObserver == null) {
             mSettingsObserver = new SettingsObserver(new Handler());
@@ -1146,6 +1177,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         // Private API call to make the shadows look better for Recents
         ThreadedRenderer.overrideProperty("ambientRatio", String.valueOf(1.5f));
+
+        // listen for NAVIGATION_BAR_ENABLED setting (per-user)
+        resetNavBarObserver();
 
         return mStatusBarView;
     }
@@ -1535,6 +1569,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     }
 
+    private void setNavBarEnabled(boolean enabled) {
+        if (enabled) {
+            addNavigationBar();
+            if (RESET_SYSTEMUI_VISIBILITY_FOR_NAVBAR) {
+                resetSystemUIVisibility();
+            }
+        } else {
+            removeNavigationBar();
+        }
+    }
+
     private void prepareNavigationBarView() {
         mNavigationBarView.reorient();
 
@@ -1558,7 +1603,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // For small-screen devices (read: phones) that lack hardware navigation buttons
     protected void addNavigationBar() {
         if (DEBUG) Log.v(TAG, "addNavigationBar: about to add " + mNavigationBarView);
-        if (mNavigationBarView == null) return;
+        if (mNavigationBarViewAttached) return;
 
         try {
             WindowManagerGlobal.getWindowManagerService()
@@ -1581,13 +1626,30 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             throw e.rethrowFromSystemServer();
         }
 
+        if (mNavigationBarView == null) {
+            // Recreate navigationbarview
+            createNavigationBarView(mContext);
+        }
         prepareNavigationBarView();
 
         mWindowManager.addView(mNavigationBarView, getNavigationBarLayoutParams());
+
+        mNavigationBarViewAttached = true;
+    }
+
+    private void removeNavigationBar() {
+        if (DEBUG) Log.v(TAG, "removeNavigationBar: about to remove " + mNavigationBarView);
+        if (!mNavigationBarViewAttached || mNavigationBarView == null) return;
+
+        mWindowManager.removeView(mNavigationBarView);
+
+        mNavigationBarView = null;
+
+        mNavigationBarViewAttached = false;
     }
 
     protected void repositionNavigationBar() {
-        if (mNavigationBarView == null || !mNavigationBarView.isAttachedToWindow()) return;
+        if (!mNavigationBarViewAttached || mNavigationBarView == null) return;
 
         prepareNavigationBarView();
 
@@ -1619,6 +1681,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         lp.setTitle("NavigationBar");
         lp.windowAnimations = 0;
         return lp;
+    }
+
+    private void resetSystemUIVisibility() {
+        checkBarModes();
+        notifyUiVisibilityChanged(mSystemUiVisibility);
     }
 
     @Override
@@ -3829,6 +3896,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     public void userSwitched(int newUserId) {
         super.userSwitched(newUserId);
         if (MULTIUSER_DEBUG) mNotificationPanelDebugText.setText("USER " + newUserId);
+        resetNavBarObserver();
         animateCollapsePanels();
         updatePublicMode();
         updateNotifications();
@@ -3856,6 +3924,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mContext.getContentResolver().registerContentObserver(
                 Settings.Secure.getUriFor(Settings.Secure.USER_SETUP_COMPLETE), true,
                 mUserSetupObserver, mCurrentUserId);
+    }
+
+    private void resetNavBarObserver() {
+        mContext.getContentResolver().unregisterContentObserver(mNavBarObserver);
+        mNavBarObserver.onChange(false);
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.NAVIGATION_BAR_ENABLED), true,
+                mNavBarObserver, mCurrentUserId);
     }
 
     /**
@@ -4115,6 +4191,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mNavigationBarView != null) {
             mWindowManager.removeViewImmediate(mNavigationBarView);
             mNavigationBarView = null;
+            mNavigationBarViewAttached = false;
         }
         if (mHandlerThread != null) {
             mHandlerThread.quitSafely();
