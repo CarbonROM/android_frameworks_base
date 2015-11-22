31a0ca68768c8f9720035283b02c016b71e5cf4b
George G <kreach3r@users.noreply.github.com>
2015-11-23 00:56:32 +0200

    Fix Fi permissions
    For Nexus 6 users, turn Fi permissions on by default (on first boot)
    
    Change-Id: I13b717c165bb585fc62b906e7bfc17778324f878

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -748,8 +748,6 @@ final class DefaultPermissionGrantPolicy {
                 grantRuntimePermissionsLPw(chromiumPackage, STORAGE_PERMISSIONS, true, userId);
             }
 
-            mService.mSettings.onDefaultRuntimePermissionsGrantedLPr(userId);
-
             // Google Account
             PackageParser.Package googleaccountPackage = getSystemPackageLPr(
                     "com.google.android.gsf.login");
@@ -869,6 +867,17 @@ final class DefaultPermissionGrantPolicy {
             if (conpro2Package != null) {
                 grantRuntimePermissionsLPw(conpro2Package, CONTACTS_PERMISSIONS, true, userId);
                 grantRuntimePermissionsLPw(conpro2Package, STORAGE_PERMISSIONS, true, userId);
+            }
+
+            // Project Fi
+            PackageParser.Package fiPackage = getDefaultProviderAuthorityPackageLPr(
+                    "com.google.android.apps.tycho", userId);
+            if (fiPackage != null) {
+                grantRuntimePermissionsLPw(fiPackage, CONTACTS_PERMISSIONS, userId);
+                grantRuntimePermissionsLPw(fiPackage, PHONE_PERMISSIONS, userId);
+                grantRuntimePermissionsLPw(fiPackage, MICROPHONE_PERMISSIONS, userId);
+                grantRuntimePermissionsLPw(fiPackage, LOCATION_PERMISSIONS, userId);
+                grantRuntimePermissionsLPw(fiPackage, SMS_PERMISSIONS, userId);
             }
             mService.mSettings.onDefaultRuntimePermissionsGrantedLPr(userId);
         }
