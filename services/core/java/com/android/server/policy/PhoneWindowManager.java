6b53383318479ba39e5819b96e7f5a9b10502a67
Randall Rushing <randall.rushing@gmail.com>
2016-08-07 12:06:07 -0700

    Hardware key disable support
    - Disable hw buttons - actions, brightness, haptic feedback, overflow menu
    
    Change-Id: Ib876f6ef50e9ba42e9aa862f50e871b3f1e39ae0

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6019,6 +6019,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mContext.sendBroadcastAsUser(errorIntent, UserHandle.CURRENT);
     }
 
+    private boolean isHwKeysDisabled() {
+        return mKeyHandler != null ? mKeyHandler.isHwKeysDisabled() : false;
+    }
+
     /** {@inheritDoc} */
     @Override
     public int interceptKeyBeforeQueueing(KeyEvent event, int policyFlags) {
@@ -6101,7 +6105,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         boolean useHapticFeedback = down
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
-                && event.getRepeatCount() == 0;
+                && event.getRepeatCount() == 0
+                && !isHwKeysDisabled();
 
         // Handle special keys.
         switch (keyCode) {
