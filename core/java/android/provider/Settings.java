0fa6b69b12fc6448bd732cff3ac7e49026f4ee61
Carlo Savignano <carlosavignano@aospa.co>
2016-09-17 04:17:40 -0400

    input: hardware buttons lights integration
    * Enable and improve partial AOSP disabled button brightness hooks.
    * Implement user-timeout/doze/dream/awake/asleep policy.
    * Addresses NOUGAT-14
    
    Change-Id: Ie1fc9d1ff98e1782bdeb3afd03b03b762f39e5fc
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4247,6 +4247,24 @@ public final class Settings {
         };
 
         /**
+         * Value for button brightness.
+         * This is an integer value in a range between 0 and 255.
+         *      0 = off
+         *    255 = default
+         *    255 = max
+         * @hide
+         */
+        public static final String BUTTON_BRIGHTNESS = "button_brightness";
+
+        /**
+         * Whether user can enable/disable button brightness.
+         * <p>
+         * Type: int (0 for false, 1 for true)
+         * @hide
+         */
+        public static final String BUTTON_BRIGHTNESS_ENABLED = "button_brightness_enabled";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4421,6 +4439,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(KEY_APP_SWITCH_DOUBLE_TAP_ACTION);
             PRIVATE_SETTINGS.add(KEY_CAMERA_LONG_PRESS_ACTION);
             PRIVATE_SETTINGS.add(KEY_CAMERA_DOUBLE_TAP_ACTION);
+            PRIVATE_SETTINGS.add(BUTTON_BRIGHTNESS);
+            PRIVATE_SETTINGS.add(BUTTON_BRIGHTNESS_ENABLED);
         }
 
         /**
