06daae26300b072a7a7180f9af4350b7c486752e
Evisceration <eviscerationls@gmail.com>
2016-09-29 05:11:26 +0200

    Add Haptic Feedback to QS tiles [1/2]
    Change-Id: If358fb624032cd24d98fe2ea0c877f4875c5c151

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3731,6 +3731,13 @@ public final class Settings {
         public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
 
         /**
+         * Whether to vibrate when quick settings tile is pressed.
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+
+        /**
          * Show the four 4g network icon instead of lte
          * @hide
          */
