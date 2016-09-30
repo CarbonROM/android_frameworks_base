fe9cdcf92fce6a8b54db6cf8b46c91be427c02a9
Carlo Savignano <carlosavignano@aospa.co>
2016-09-30 15:56:48 -0400

    SystemUI: alert slider integration
    * Volume panel UI adjustements for alert slider devices.
    * Add quick settings tile for extra settings within detail view, disabled when zen is off.
    * Silent state: Total silence / Alarms only.
    * Priority state: Near-stock without automatic and times' rules, permanent introduction
      with customize button and exposed only events and reminders toggles).
    * Set DND tile visibility only when registering volume panel.
    * Add missing priority icon for zen footer, both w/ and w/o alert slider.
    
    TODO> Merge with AOSP Do Not Disturb tile and policy.
    
    Change-Id: I0a5240e0a237f5fd89468dc5961b1c1a6487228c
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogController.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogController.java
@@ -42,6 +42,7 @@ import android.os.RemoteException;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.service.notification.Condition;
+import android.text.TextUtils;
 import android.util.Log;
 import android.util.SparseArray;
 
@@ -104,6 +105,7 @@ public class VolumeDialogController {
     private boolean mDestroyed;
     private VolumePolicy mVolumePolicy;
     private boolean mShowDndTile = true;
+    private boolean mHasAlertSlider = false;
 
     private ToneGenerator mToneGenerators[];
 
@@ -125,6 +127,9 @@ public class VolumeDialogController {
         mStreamTitles = mContext.getResources().getStringArray(R.array.volume_stream_titles);
         mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
         mHasVibrator = mVibrator != null && mVibrator.hasVibrator();
+        mHasAlertSlider = mContext.getResources().getBoolean(com.android.internal.R.bool.config_hasAlertSlider)
+                && !TextUtils.isEmpty(mContext.getResources().getString(com.android.internal.R.string.alert_slider_state_path))
+                && !TextUtils.isEmpty(mContext.getResources().getString(com.android.internal.R.string.alert_slider_uevent_match_path));
     }
 
     public AudioManager getAudioManager() {
@@ -143,7 +148,7 @@ public class VolumeDialogController {
             return;
         }
         setVolumePolicy(mVolumePolicy);
-        showDndTile(mShowDndTile);
+        showDndTile(mShowDndTile && !mHasAlertSlider);
         try {
             mMediaSessions.init();
         } catch (SecurityException e) {
