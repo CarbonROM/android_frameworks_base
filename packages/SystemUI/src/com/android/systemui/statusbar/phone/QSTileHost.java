1d514461561d125a934299f327b47b86cf6a955c
Carlo Savignano <carlosavignano@aospa.co>
2016-09-30 15:56:48 -0400

    SystemUI: alert slider integration
    * Volume panel UI adjustements for alert slider devices.
    * Add quick settings tile for extra settings within detail view, disabled when zen is off.
    * Silent state: Total silence / Alarms only.
    * Priority state: Near-stock without automatic and times' rules, permanent introduction
      with customize button and exposed only events and reminders toggles).
    * Set DND tile visibility only when registering volume panel.
    * Add missing priority icon for zen footer, both w/ and w/o alert slider.
    
    TODO> Merge with AOSP Do Not Disturb tile and policy.
    
    Change-Id: I0a5240e0a237f5fd89468dc5961b1c1a6487228c
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.external.TileServices;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AndroidAutoTile;
+import com.android.systemui.qs.tiles.AlertSliderTile;
 import com.android.systemui.qs.tiles.BatteryTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.BrightnessTile;
@@ -451,7 +452,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("sound")) return new SoundTile(this);
         else if (tileSpec.equals("nfc")) return new NfcTile(this);
         else if (tileSpec.equals("music")) return new MusicTile(this);
-
+        else if (tileSpec.equals("alertslider")) return new AlertSliderTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
