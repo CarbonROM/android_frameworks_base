1f654afeb60a54bf2151cda4b6403e9a0c936646
rogersb11 <brettrogers11@gmail.com>
2016-10-21 15:07:23 +0200

    [1/2] FWB: The Return of the Ticker
    Return of ticker work done by @rogersb11
    Included are on and off switch from @Jubakuba
    
    Custom color selection for Ticker text and icons also font and size
    
    AICPfy:
    - Bring it to AICP source
    - No need to update API
    
    Change-Id: I6590759af0bdda9bba4f60e9c3f768b065b856b7
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4281,6 +4281,36 @@ public final class Settings {
         // Settings moved to Settings.Secure
 
         /**
+         * Wether to show the ticker on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_TICKER = "status_bar_show_ticker";
+
+        /**
+         * Color of the status bar notification text
+         * @hide
+         */
+        public static final String STATUS_BAR_TICKER_TEXT_COLOR = "status_bar_ticker_text_color";
+
+        /**
+         * Ticker Icon Color
+         * @hide
+         */
+        public static final String STATUS_BAR_TICKER_ICON_COLOR = "status_bar_ticker_icon_color";
+
+        /**
+         * Ticker Font Style
+         * @hide
+         */
+        public static final String STATUS_BAR_TICKER_FONT_STYLE = "status_bar_ticker_font_style";
+
+        /**
+         * Ticker Text Size
+         * @hide
+         */
+        public static final String STATUS_BAR_TICKER_FONT_SIZE = "status_bar_ticker_font_size";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#ADB_ENABLED}
          * instead
          */
