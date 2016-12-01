d5f6ee73a8dc224c4fea76a4cf0fb833bb06fc57
David Vandenburgh <davidvandenburgh@linux.com>
2016-12-01 05:49:18 -0500

    Alert Slider for Oneplus devices Conflicts: 	core/res/res/values/strings.xml 	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java 	packages/SystemUI/src/com/android/systemui/volume/ZenFooter.java
    Conflicts:
    	core/res/res/values/symbols.xml
    	packages/SystemUI/res/values/config.xml
    	services/core/java/com/android/server/policy/PhoneWindowManager.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/strings.xml
    	core/res/res/values/symbols.xml
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/custom_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Change-Id: I06462e2d1982762c77fbb26395088a5757c6bfc7

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -901,6 +901,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.KEYGUARD_TOGGLE_TORCH), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.ALERT_SLIDER_ORDER), false, this,
+                    UserHandle.USER_ALL);
             updateSettings();
         }
 
