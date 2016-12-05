1ab4c6124763610f27631a1f4d1c05d3dd975940
Willi Ye <williye97@gmail.com>
2016-12-05 00:47:07 -0500

    Fwb: Port PA battery circle style
    This commit adds the infamous PA battery circle style from Marshmallow.
    The original implementation replaces CM's circle battery style. But, why
    not make it an option alongside? The code has been broken up and refractored
    to be a standalone battery style to be chosen in the Styles menu from Settings.
    
    No clue if this works, but why not give it a try just for fun? :)
    
    Also a patch is included to disable Bolt checks, cherry-picked from the
    original styles commit
    here: AOSPA/android_frameworks_base/commit/c5ef709a286d92d78f8445c53c1a8c7a4a9456ca
    
    Change-Id: I84711aa5c06d36c29437876ffebe72bb9b4632d9
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -68,6 +68,7 @@ public class BatteryMeterDrawable extends Drawable implements
     public static final int BATTERY_STYLE_HIDDEN    = 4;
     public static final int BATTERY_STYLE_LANDSCAPE = 5;
     public static final int BATTERY_STYLE_TEXT      = 6;
+    public static final int BATTERY_STYLE_TEXT      = 7;
 
     private final int[] mColors;
     private final int mIntrinsicWidth;
@@ -493,6 +494,8 @@ public class BatteryMeterDrawable extends Drawable implements
                 return R.drawable.ic_battery_circle;
             case BATTERY_STYLE_PORTRAIT:
                 return R.drawable.ic_battery_portrait;
+            case BATTERY_STYLE_SOLID:
+                return R.drawable.ic_battery_solid;
             default:
                 return 0;
         }
@@ -506,6 +509,8 @@ public class BatteryMeterDrawable extends Drawable implements
                 return R.style.BatteryMeterViewDrawable_Circle;
             case BATTERY_STYLE_PORTRAIT:
                 return R.style.BatteryMeterViewDrawable_Portrait;
+            case BATTERY_STYLE_SOLID:
+                return R.style.BatteryMeterViewDrawable_Solid;
             default:
                 return R.style.BatteryMeterViewDrawable;
         }
@@ -550,7 +555,10 @@ public class BatteryMeterDrawable extends Drawable implements
         final float textSize;
         switch(mStyle) {
             case BATTERY_STYLE_CIRCLE:
-                textSize = widthDiv2 * 0.8f;
+                textSize = widthDivCircle* 1.2f;
+                break;
+            case BATTERY_STYLE_SOLID:
+                textSize = widthDiv2 * 1.0f;
                 break;
             case BATTERY_STYLE_LANDSCAPE:
                 textSize = widthDiv2 * 1.0f;
