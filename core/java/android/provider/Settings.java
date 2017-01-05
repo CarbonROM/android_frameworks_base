f1e1c80a21b6aad1c51d71bef4ba64ffd21cb53c
Carlo Savignano <carlosavignano@aospa.co>
2017-01-05 13:11:56 +0100

    SystemUI: support swapping navigation bar keys order
    * Use new API from SystemUI tuner that allows inflate
      custom layout on the fly.
    
    TODO> Allow SystemUI Tuner to override this when enabled and changes
          are made after enabling it. When disabling the tuner then use
          default arrangement and apply swap only.
    
    Change-Id: Ib1993140eb1bf7dc0017d75015516db95ce70d9a
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4506,6 +4506,14 @@ public final class Settings {
         public static final String GESTURE_DOUBLE_TAP = "gesture_double_tap";
 
         /**
+         * Whether user can swap navigation lateral keys.
+         * <p>
+         * Type: int (0 for false, 1 for true)
+         * @hide
+         */
+        public static final String SWAP_NAVIGATION_KEYS = "swap_navigation_keys";
+
+       /**
          * Controls whether draw V gesture is enabled.
          * @hide
          */
