23fc1b6b4b4be1f4d591c585a536c9879684b223
maxwen <max.weninger@gmail.com>
2017-01-21 10:11:52 +0100

    Dashboard tile layout config [2/2]
    Change-Id: I387b0ecfec5bd71e8c1176b06a83796d09aba3e4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3944,6 +3944,12 @@ public final class Settings {
         public static final String QS_ROWS_LANDSCAPE = "qs_rows_landscape";
 
         /**
+         * Dashboard columns in settings
+         * @hide
+         */
+        public static final String DASHBOARD_COLUMNS = "dashboard_columns";
+
+        /**
          * Whether to hide the clock, show it in the right or left
          * position or show it in the center
          * 0: don't show the clock
