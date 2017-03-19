76fa42a65cfdcce89e6f1809a1d4f766a2c00800
Christian Oder <myself5@carbonrom.org>
2017-03-19 23:46:22 +0100

    Revert "SystemUI: Statusbar: Add HSPA+ icons"
    This reverts commit bd41c07ff97d5a260c133251538f9fdfc05366ff.
    
    Change-Id: If2c35bd7a5b828c88392a7cd181f51a427c98e88

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -787,7 +787,6 @@ public class NetworkControllerImpl extends BroadcastReceiver
                             datatype.equals("e") ? TelephonyIcons.E :
                             datatype.equals("g") ? TelephonyIcons.G :
                             datatype.equals("h") ? TelephonyIcons.H :
-                            datatype.equals("hp") ? TelephonyIcons.HP :
                             datatype.equals("lte") ? TelephonyIcons.LTE :
                             datatype.equals("lte+") ? TelephonyIcons.LTE_PLUS :
                             datatype.equals("roam") ? TelephonyIcons.ROAMING :
@@ -864,7 +863,6 @@ public class NetworkControllerImpl extends BroadcastReceiver
         boolean alwaysShowCdmaRssi = false;
         boolean hideLtePlus = false;
         boolean hspaDataDistinguishable;
-	boolean hspapDataDistinguishable;
 
         static Config readConfig(Context context) {
             Config config = new Config();
@@ -876,8 +874,6 @@ public class NetworkControllerImpl extends BroadcastReceiver
             config.hspaDataDistinguishable =
                     res.getBoolean(R.bool.config_hspa_data_distinguishable);
             config.hideLtePlus = res.getBoolean(R.bool.config_hideLtePlus);
-            config.hspapDataDistinguishable =
-                    res.getBoolean(R.bool.config_hspap_data_distinguishable);
             return config;
         }
     }
