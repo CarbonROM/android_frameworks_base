93991a99076c50d91f6b9a27657e81cdf775a58b
Roman Birg <roman@cyngn.com>
2017-04-24 22:43:54 +0200

    [QUICK SETTINGS] Add NFC Tile
    edited for Nougat by @xyyx
    
    Fix NFC tile disappearing randomly
    
    While the system is booting up the NfcAdapter.getNfcAdapter(Context context)
    might throw an UnsupportedOperationException while retrieving the NFC service
    
    Because the NfcManager catches this exception and sets its private
    mAdapter variable null, subsequent calls to the manager will yield only
    null and therefore the NFC tile will never be able to obtain an instance
    of an NfcAdapter.
    
    Change-Id: Ia5a4cc24346b2dd5ebe423832f986bb72e57096b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -55,6 +55,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SoundTile;
@@ -443,6 +444,8 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("androidauto")) return new AndroidAutoTile(this);
         else if (tileSpec.equals("sync")) return new SyncTile(this);
         else if (tileSpec.equals("sound")) return new SoundTile(this);
+        else if (tileSpec.equals("nfc")) return new NfcTile(this);
+
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
