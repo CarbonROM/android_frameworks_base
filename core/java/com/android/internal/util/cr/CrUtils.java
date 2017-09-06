ad3858cf975a59fe0577dfd48c4d940f6ffc49c4
bigrushdog <randall.rushing@gmail.com>
2017-09-06 13:09:52 +0200

    Add api to take screenshots
    abc: extracted from DUI
    add a public call to Utils
    adapted for partial screenshot
    
    Change-Id: Ia569f20a68632f71a0d27f1620a0f20862edeb14

--- core/java/com/android/internal/util/cr/CrUtils.java
+++ core/java/com/android/internal/util/cr/CrUtils.java
@@ -35,6 +35,8 @@ import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 
 import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
@@ -43,6 +45,9 @@ import java.util.Locale;
 import java.util.Arrays;
 
 public class CrUtils {
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     public static boolean isChineseLanguage() {
        return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
@@ -205,5 +210,15 @@ public class CrUtils {
         // Default to celsius if can't access MCC
         return !TextUtils.isEmpty(networkOperator) && Arrays.asList(mcc).contains(
                 networkOperator.substring(0, /*Filter only 3 digits*/ 3));
+    }
+
+    // Method to take screenshots
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
