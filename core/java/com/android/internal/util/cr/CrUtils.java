08dc63645c6e253ab1208573feedeab3a88c6eef
ezio84 <brabus84@gmail.com>
2017-09-09 22:15:04 +0800

    base: Add a sendKeycode api to CrUtils
    code borrowed from com.android.systemui.statusbar.policy.KeyButtonView
    
    Change-Id: I15e667b1b1f19abeccfe2b6f85868de1a90682e9

--- core/java/com/android/internal/util/cr/CrUtils.java
+++ core/java/com/android/internal/util/cr/CrUtils.java
@@ -21,11 +21,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.hardware.input.InputManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.BatteryManager;
 import android.os.PowerManager;
 import android.os.RemoteException;
@@ -34,6 +37,9 @@ import android.os.SystemClock;
 import android.net.ConnectivityManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.view.InputDevice;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
 
 import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
@@ -127,6 +133,30 @@ public class CrUtils {
 
     public static void toggleCameraFlash() {
         FireActions.toggleCameraFlash();
+    }
+
+    public static void sendKeycode(int keycode) {
+        long when = SystemClock.uptimeMillis();
+        final KeyEvent evDown = new KeyEvent(when, when, KeyEvent.ACTION_DOWN, keycode, 0,
+                0, KeyCharacterMap.VIRTUAL_KEYBOARD, 0,
+                KeyEvent.FLAG_FROM_SYSTEM | KeyEvent.FLAG_VIRTUAL_HARD_KEY,
+                InputDevice.SOURCE_KEYBOARD);
+        final KeyEvent evUp = KeyEvent.changeAction(evDown, KeyEvent.ACTION_UP);
+         final Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                InputManager.getInstance().injectInputEvent(evDown,
+                        InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
+            }
+        });
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                InputManager.getInstance().injectInputEvent(evUp,
+                        InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
+            }
+        }, 20);
     }
 
     public static void goToSleep(Context context) {
