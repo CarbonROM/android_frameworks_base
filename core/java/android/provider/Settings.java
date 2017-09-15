3c633c8f1a1dd904e5ad84e7efd091534986c471
kameron19921016 <kameron19921016@yahoo.com>
2017-09-15 09:13:30 +0800

    base: Add Volume Key Answer [1/2]
    Change-Id: Icd70db06699da3c81d7c596b4305a8384774c055
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4330,6 +4330,12 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
         /**
+         * volume answer
+         * @hide
+         */
+        public static final String ANSWER_VOLUME_BUTTON_BEHAVIOR_ANSWER = "call_volume_answer";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
