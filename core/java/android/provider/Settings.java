d247d156266000e69eebff43548b80c3356beec8
Carsten Hauge <carsten.hauge@sonymobile.com>
2017-09-21 22:45:21 +0200

    SystemUI: Vibrate on plug [1/2]
    ---
    abc ezio84: remove sound part and vibrate also when normal ringer mode
    use new VibrationEffect api 26
    play vibration also on disconnected charge
    add Settings part
    get services on init
    ---
    
    original commit: https://android-review.googlesource.com/#/c/platform/frameworks/base/+/243420/
    
    Edits for DU by @nathanchance:
    - Add unplugged function so vibrate can fire without sound
    - Fire vibration before playing a sound
    - Vibrate in all modes other than total silence
    
    Change-Id: Ic0e09d033b781e40f030e6e62e5fec0433076732
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4279,6 +4279,12 @@ public final class Settings {
         public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
         /**
+         * Whether to vibrate on power connection or disconnection
+         * @hide
+         */
+        public static final String VIBRATION_ON_CHARGE_STATE_CHANGED = "vibration_on_charge_state_changed";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
