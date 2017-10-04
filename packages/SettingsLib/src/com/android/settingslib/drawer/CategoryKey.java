e1256d6401e12c25a53b3b2a4df4284098f68642
gtpitch <bmontagu@gmail.com>
2017-10-03 22:34:23 -0400

    base: add CarbonFibers category key
    Change-Id: I8fd61b1bf265e0f70433d4b8176cbdbf7f674218
    Signed-off-by: gtpitch <bmontagu@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
@@ -51,6 +51,8 @@ public final class CategoryKey {
     public static final String CATEGORY_GESTURES = "com.android.settings.category.ia.gestures";
     public static final String CATEGORY_NIGHT_DISPLAY =
             "com.android.settings.category.ia.night_display";
+    public static final String CATEGORY_CARBONFIBERS =
+             "com.android.settings.category.ia.fibers";
 
     public static final Map<String, String> KEY_COMPAT_MAP;
 
