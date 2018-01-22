e51cfad4d0755a43f6fd8290189d9ad0c33a1198
ezio84 <brabus84@gmail.com>
2018-01-22 15:20:58 +0100

    Allow to dismiss notifications with back FP left/right swipe [1/2]
    if the panel is expanded.
    
    The swipe animation will follow the swipe gesture direction.
    
    Change-Id: I358ba07fa7724f9549145c87a1440916c469ca53

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -212,6 +212,7 @@ import com.android.systemui.statusbar.notification.logging.NotificationLogger;
 import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow;
 import com.android.systemui.statusbar.notification.row.NotificationGutsManager;
 import com.android.systemui.statusbar.notification.stack.NotificationListContainer;
+import com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayout;
 import com.android.systemui.statusbar.phone.UnlockMethodCache.OnUnlockMethodChangedListener;
 import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.BatteryController.BatteryStateChangeCallback;
@@ -410,6 +411,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     private View mReportRejectedTouch;
 
     private boolean mExpandedVisible;
+
+    private boolean mFpDismissNotifications;
 
     private final int[] mAbsPos = new int[2];
     private final ArrayList<Runnable> mPostCollapseRunnables = new ArrayList<>();
@@ -1889,6 +1892,15 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mNotificationPanel.flingSettings(0 /* velocity */,
                         NotificationPanelView.FLING_EXPAND);
                 mMetricsLogger.count(NotificationPanelView.COUNTER_PANEL_OPEN_QS, 1);
+            }
+        } else if (mFpDismissNotifications && (KeyEvent.KEYCODE_SYSTEM_NAVIGATION_LEFT == key
+                || KeyEvent.KEYCODE_SYSTEM_NAVIGATION_RIGHT == key)) {
+            if (!mNotificationPanel.isFullyCollapsed() && !mNotificationPanel.isExpanding()){
+                mMetricsLogger.action(MetricsEvent.ACTION_DISMISS_ALL_NOTES);
+                ((NotificationStackScrollLayout)mStackScroller).clearNotifications(
+                        ((NotificationStackScrollLayout)mStackScroller).ROWS_ALL/*all notifications*/,
+                        true/*collapse the panel*/,
+                        KeyEvent.KEYCODE_SYSTEM_NAVIGATION_LEFT == key/*left swipe animation*/);
             }
         }
 
@@ -3853,6 +3865,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -3861,12 +3876,17 @@ public class StatusBar extends SystemUI implements DemoMode,
                   Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN))) {
               setStatusBarWindowViewOptions();
           }
+          if (uri.equals(Settings.Secure.getUriFor(
+                  Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS))) {
+              setFpToDismissNotifications();
+          }
             update();
         }
 
         public void update() {
             setHideArrowForBackGesture();
             setStatusBarWindowViewOptions();
+            setFpToDismissNotifications();
         }
     }
     private void setHideArrowForBackGesture() {
@@ -3879,6 +3899,12 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mStatusBarWindow != null) {
             mStatusBarWindow.setStatusBarWindowViewOptions();
         }
+    }
+
+    private void setFpToDismissNotifications() {
+        mFpDismissNotifications = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS, 0,
+                UserHandle.USER_CURRENT) == 1;
     }
 
     public int getWakefulnessState() {
