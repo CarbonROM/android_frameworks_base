6f4bab49750111171a3e9f6a37d958a3987dfb7f
Alex Cruz <du.alexcruz@gmail.com>
2018-01-23 18:55:08 -0500

    Theme DUI dialogs & things [2/2]
    Change-Id: I896f4a9b5f9354e025f3204e429034ac4fe28f3b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4983,6 +4983,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                         useDarkTheme, mCurrentUserId);
                 mOverlayManager.setEnabled("com.android.settings.theme.dark",
                         useDarkTheme, mCurrentUserId);
+                mOverlayManager.setEnabled("com.android.dui.theme.dark",
+                        useDarkTheme, mCurrentUserId);
                 if (useDarkTheme) {
                     unloadStockDarkTheme();
                 }
