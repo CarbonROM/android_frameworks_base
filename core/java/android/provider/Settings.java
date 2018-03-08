dcc0911afcb24513e1ac4034c2c7be4a912afbb2
Alex <mazdarider23@gmail.com>
2018-03-08 00:12:15 +0000

    fwb: Immersive Recents [1/2]
    0 - Default AOSP
    1 - Fullscreen
    2 - Statusbar only
    3 - NavBar only
    
    Change-Id: I471051b5ae1ae72863960da3297cdf7ad3b7da49

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4249,6 +4249,17 @@ public final class Settings {
         public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
 
         /**
+         * Immersive recents options
+         *
+         * 0 = Default AOSP look
+         * 1 = Statusbar only
+         * 2 = Navbar only
+         * 3 = Fullscreen
+         * @hide
+         */
+        public static final String IMMERSIVE_RECENTS = "immersive_recents";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
