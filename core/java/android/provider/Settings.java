f4ee841790da91679ae63676b332caca101b7109
Brett Montague <bmontagu@gmail.com>
2018-03-17 12:59:11 -0400

    base: add toggle to allow untrusted overlays to be installed (1/2)
    This patch allows the user to select if they want theme overlays
    that are not signed by system to be installed
    
    Change-Id: Id16a3a8b154af1998eb3052cfc00ecf981f8d990
    Signed-off-by: Brett Montague <bmontagu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7675,6 +7675,13 @@ public final class Settings {
         public static final String FACE_AUTO_UNLOCK = "face_auto_unlock";
 
         /**
+         * Boolean value whether to allow untrusted overlays to be installed
+         *
+         * @hide
+         */
+        public static final String UNTRUSTED_OVERLAY_TOGGLE = "untrusted_overlay_toggle";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
