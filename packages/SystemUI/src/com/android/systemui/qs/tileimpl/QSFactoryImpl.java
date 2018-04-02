cd99e726d92a8b9c29e065f4ef3e350c5d42497e
darjwx <darjwx@gmail.com>
2018-04-02 16:33:18 +0200

    SystemUI: QS: add power tile
    * Single press changes between modes: reboot and shut down
    * Long press fires the selected mode
    * Long press in reboot mode fires additional option
    (system, recovery and bootloader)
    
    * Icons from: https://material.io/icons/
    
    * @adinkwok clean up
    
    Change-Id: Id1a10795fb067419a834f4255fe92a8ec1dfccf1

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -38,6 +38,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.NotificationTile;
+import com.android.systemui.qs.tiles.PowerTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -79,6 +80,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("screenrecord")) return new ScreenrecordTile(mHost);
         else if (tileSpec.equals("notification")) return new NotificationTile(mHost);
         else if (tileSpec.equals("smartpixels")) return new SmartPixelsTile(mHost);
+        else if (tileSpec.equals("power")) return new PowerTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
