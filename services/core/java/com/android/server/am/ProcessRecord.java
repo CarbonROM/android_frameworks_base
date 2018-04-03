7cf99e93089b35852aa67b58a69a0644fb3f0022
Preeti Nagar <pnagar@codeaurora.org>
2018-04-03 16:34:25 +0530

    SEEMP: framework instrumentation and AppProtect features
    - Changes for Android-P porting
    - Instrument framework files to log events of interest for QSSP.
    - Fix the String out of boundary.
    - Add optional package verifier to PackageManagerService
      - In additional to the required and sufficient package verifiers,
        add support for an optional verifier in PackageManager. The
        optional verifier will act as a required verifier if a required
        verifier is not found in the system. It will act as a secondary
        verifier if a required verifier is present.
    - Add auth framework for outgoing SMS messages.
      - Add the APIs required to register an authorization agent in order
        to allow/reject outgoing SMS messages and the core service that
        implements the sms security model.
    - Do not connect to seempdw socket in zygote
      - Do not connect to seempdw socket in zygote. The socket
        initialization is skipped if the parent process id is 1, which is
        always the case for zygote.
    - Moved instrumentation point to PackageInstaller and some
      minor fixes.
    - Add action for optional package verifier along other legacy
      actions to handle tightened StrictMode policy in Android P.
    
    Conflicts:
    	Android.mk
    	core/java/android/accounts/AccountManager.java
    	core/java/android/app/Instrumentation.java
    	core/java/android/bluetooth/BluetoothAdapter.java
    	core/jni/Android.mk
    	core/res/res/values/config.xml
    	core/res/res/values/symbols.xml
    	services/core/java/com/android/server/pm/PackageManagerService.java
    	telephony/java/android/telephony/Telephony.java
    	tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/view/WindowManagerImpl.java
    	core/java/android/app/Instrumentation.java
    	core/res/res/values/config.xml
    	core/res/res/values/symbols.xml
    
    Change-Id: I38a8ace9943fae0ad2ac7bc2932f3deb790359a6
    CRs-Fixed: 2219016

--- services/core/java/com/android/server/am/ProcessRecord.java
+++ services/core/java/com/android/server/am/ProcessRecord.java
@@ -600,6 +600,18 @@ class ProcessRecord implements WindowProcessListener {
     }
 
     public void makeActive(IApplicationThread _thread, ProcessStatsService tracker) {
+        String seempStr = "app_uid=" + uid
+                            + ",app_pid=" + pid + ",oom_adj=" + curAdj
+                            + ",setAdj=" + setAdj + ",hasShownUi=" + (hasShownUi ? 1 : 0)
+                            + ",cached=" + (cached ? 1 : 0)
+                            + ",fA=" + (foregroundActivities ? 1 : 0)
+                            + ",fS=" + (foregroundServices ? 1 : 0)
+                            + ",systemNoUi=" + (systemNoUi ? 1 : 0)
+                            + ",curSchedGroup=" + curSchedGroup
+                            + ",curProcState=" + curProcState + ",setProcState=" + setProcState
+                            + ",killed=" + (killed ? 1 : 0) + ",killedByAm=" + (killedByAm ? 1 : 0)
+                            + ",debugging=" + (debugging ? 1 : 0);
+        android.util.SeempLog.record_str(386, seempStr);
         if (thread == null) {
             final ProcessState origBase = baseProcessTracker;
             if (origBase != null) {
@@ -633,6 +645,18 @@ class ProcessRecord implements WindowProcessListener {
     }
 
     public void makeInactive(ProcessStatsService tracker) {
+        String seempStr = "app_uid=" + uid
+                            + ",app_pid=" + pid + ",oom_adj=" + curAdj
+                            + ",setAdj=" + setAdj + ",hasShownUi=" + (hasShownUi ? 1 : 0)
+                            + ",cached=" + (cached ? 1 : 0)
+                            + ",fA=" + (foregroundActivities ? 1 : 0)
+                            + ",fS=" + (foregroundServices ? 1 : 0)
+                            + ",systemNoUi=" + (systemNoUi ? 1 : 0)
+                            + ",curSchedGroup=" + curSchedGroup
+                            + ",curProcState=" + curProcState + ",setProcState=" + setProcState
+                            + ",killed=" + (killed ? 1 : 0) + ",killedByAm=" + (killedByAm ? 1 : 0)
+                            + ",debugging=" + (debugging ? 1 : 0);
+        android.util.SeempLog.record_str(387, seempStr);
         thread = null;
         mWindowProcessController.setThread(null);
         final ProcessState origBase = baseProcessTracker;
