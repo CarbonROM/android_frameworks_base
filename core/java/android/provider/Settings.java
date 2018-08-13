6b12a1ca3c4dcd18225e211b370e903c700faf3e
darjwx <darjwx@gmail.com>
2018-08-13 01:46:53 -0700

    SystemUI: QS: add power tile
    * Single press changes between modes: reboot, shut down and preferences
    * Long press fires the selected mode
    * The reboot mode is configurable in preferences
      (system, recovery and bootloader)
    * The tile saves your preferences
    * Long pressing reboot will only reboot to system if keyguard is locked
    
    * Icons from: https://material.io/icons/
    
    * Adin Kwok: clean up and help with the storage part
    * frap129: clean up
    * Alex Cruz: a few suggestions
    
    Change-Id: Id1a10795fb067419a834f4255fe92a8ec1dfccf1
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4581,6 +4581,12 @@ public final class Settings {
         public static final String GLOBAL_ACTIONS_FLASHLIGHT = "global_actions_flashlight";
 
         /**
+         * Storage for the reboot option selected in Power tile
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_REBOOT_OPTIONS = "quick_settings_reboot_options";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -39,6 +39,7 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.PowerTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
@@ -176,6 +177,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SmartPixelsTile(mHost);
             case "caffeine":
                 return new CaffeineTile(mHost);
+            case "power":
+                return new PowerTile(mHost);
         }
 
         // Intent tiles.
