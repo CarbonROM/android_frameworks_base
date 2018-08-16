d63bef1f5a8d1ca8b7039aba55eba3b88f8ff9df
maxwen <max.weninger@gmail.com>
2018-08-16 20:53:46 +0200

    base: Add navigation bar visibility control [1/2]
    Change-Id: Iefc248dffd5d267ad801ee7aee499de1a9f8d476
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/com/android/internal/util/cr/CrUtils.java
+++ core/java/com/android/internal/util/cr/CrUtils.java
@@ -27,6 +27,7 @@ import android.hardware.camera2.CameraManager;
 import android.net.ConnectivityManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.util.DisplayMetrics;
 
 import com.android.internal.statusbar.IStatusBarService;
@@ -163,5 +164,31 @@ public class CrUtils {
         DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
         float px = 24 * (metrics.densityDpi / 160f);
         return result > Math.round(px);
+    }
+
+    // Method to hide/show navbar
+    public static boolean deviceSupportNavigationBar(Context context) {
+        return deviceSupportNavigationBarForUser(context, UserHandle.USER_CURRENT);
+    }
+
+    public static boolean deviceSupportNavigationBarForUser(Context context, int userId) {
+        final boolean showByDefault = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar);
+        final int hasNavigationBar = Settings.System.getIntForUser(
+                context.getContentResolver(),
+                Settings.System.FORCE_SHOW_NAVBAR, -1, userId);
+
+        if (hasNavigationBar == -1) {
+            String navBarOverride = SystemProperties.get("qemu.hw.mainkeys");
+            if ("1".equals(navBarOverride)) {
+                return false;
+            } else if ("0".equals(navBarOverride)) {
+                return true;
+            } else {
+                return showByDefault;
+            }
+        } else {
+            return hasNavigationBar == 1;
+        }
     }
 }
