b0be3c916c2130a02b28f0e5d59719b94d1a50c2
bigrushdog <randall.rushing@gmail.com>
2018-11-13 05:30:20 +0000

    base: Fonts: Update keyguard when font changes
    removed unused variables
    
    CarbonROM Edits:
    * Adjust for KeyguardClockSwitch
    * Extend KeyguardClockSwitch and ClockPlugins to allow setting Typeface
    
    Change-Id: If9305cbdeff2dad8e7a4bbcd073d94e20d9720b0

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -34,6 +34,7 @@ import android.content.res.TypedArray;
 import android.database.ContentObserver;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.UserHandle;
@@ -97,7 +98,7 @@ public class BatteryMeterView extends LinearLayout implements
     private final ImageView mBatteryIconView;
     private final CurrentUserTracker mUserTracker;
     private TextView mBatteryPercentView;
-
+    private static final String FONT_FAMILY = "sans-serif";
     private BatteryController mBatteryController;
     private SettingObserver mSettingObserver;
     private final @StyleRes int mPercentageStyleId;
