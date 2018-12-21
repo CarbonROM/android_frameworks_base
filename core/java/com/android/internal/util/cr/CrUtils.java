a51ca60a940bc0e8f884ed10aaded20d1c9da302
ezio84 <brabus84@gmail.com>
2018-12-20 20:05:15 -0600

    Navbar: Add Expand notifications action [1/2]
    Change-Id: I70878ba8815af82edbe856ef39c832e0b8206a8b

--- core/java/com/android/internal/util/cr/CrUtils.java
+++ core/java/com/android/internal/util/cr/CrUtils.java
@@ -155,6 +155,10 @@ public class CrUtils {
         FireActions.clearAllNotifications();
     }
 
+    public static void toggleNotifications() {
+        FireActions.toggleNotifications();
+    }
+
     private static final class FireActions {
         private static IStatusBarService mStatusBarService = null;
         private static IStatusBarService getStatusBarService() {
@@ -183,6 +187,17 @@ public class CrUtils {
             if (service != null) {
                 try {
                     service.onClearAllNotifications(ActivityManager.getCurrentUser());
+                } catch (RemoteException e) {
+                    // do nothing.
+                }
+            }
+        }
+
+        public static void toggleNotifications() {
+            IStatusBarService service = getStatusBarService();
+            if (service != null) {
+                try {
+                    service.expandNotificationsPanel();
                 } catch (RemoteException e) {
                     // do nothing.
                 }
