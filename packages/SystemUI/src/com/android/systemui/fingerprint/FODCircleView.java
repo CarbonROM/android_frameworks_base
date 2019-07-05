e3e58f43250d9b7c68593e7b07354ea18c711834
Christian Oder <myself5@carbonrom.org>
2019-07-06 00:41:08 +0200

    Revert "FOD: dont show FP icons if FOD is in use"
    This reverts commit 1259af457c0bb1e290c9920842ba8e6eb5644e62.
    
    Change-Id: I19f9f083da25dd70261f53df1c663a8a1adcf832

--- packages/SystemUI/src/com/android/systemui/fingerprint/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/fingerprint/FODCircleView.java
@@ -316,12 +316,4 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         mWM.removeView(this);
         viewAdded = false;
     }
-
-    private void setCustomIcon(){
-        if (mIsDreaming && !mIsPulsing) {
-            setImageResource(R.drawable.fod_icon_empty);
-            return;
-        }
-        setImageResource(R.drawable.fod_icon_default);
-    }
 }
