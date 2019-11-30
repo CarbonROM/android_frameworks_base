74f4050a809102b44b48d9db2eb42c60c4eaa041
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    SystemUI: Bring back good ol' circle battery style
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   2019-11-30 13:41:05 +0100
        SystemUI: Bring back good ol' circle battery style
    
        * This code is based on the old Java implementation and converted to Kotlin
    
        * Instead of creating different style within the same drawable
          create one on its own, this makes the code more readable and future proof
    
        Change-Id: I23798426e603b277408fb63f4898b385027e7eeb
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   2021-04-08 21:41:24 +0100
        SystemUI: Fix status bar battery icon title
    
        The wrong string was being picked up which is not capitalized
        and not even localized. Use the already localized string
        from SystemUI package.
    
        Change-Id: I4be427c95a0fb98c77d450e0791bb8e3442ed3a1
    
    Author: althafvly <althafvly@gmail.com>
    Date:   Mon Jul 11 15:13:40 2022 +0530
        SystemUI: Redo circle battery style
    
        - Now shows up on QuickSettings panel.
        - Fixes issues with battery circle style on theme and
          ui mode change and statusbar icon toggle.
        - Fixes wrong tint color with circle battery icon.
        - Re-evaluating system theme to avoid delay in circle
          battery tint color change.
    
        Change-Id: Id90f85e47ce4b9811e9bb67b91169997020300d8
    
    Change-Id: If9c55253c8e8786b4998632241a872104b7e7e8e
    
    Battery options cleanup [1/2]
    
    previously there was some logic in the Settings part that was setting text percentage style
    even for the hidden style option to load the needed values in BatteryMeterView.
    That made the code really hard to read and understand, and needed an additional system setting value.
    Now we move the proper logic to BatteryMeterView so there's no confusion and possible glitches due to settings preferences loading,
    This commit also fixes at least one glitch: while charging, set hidden style, then exit from battery options menu, then enter it again.
    Before the commit it was setting the text style back.
    
    Change-Id: Ic56196870427d11895e2d9e79413d0fcd673b7d3
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/6747
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5524,6 +5524,21 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Battery style
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
+          * Statusbar Battery %
+          * 0: Hide the battery percentage
+          * 1: Display the battery percentage inside the icon
+          * 2: Display the battery percentage next to Icon
+          * @hide
+          */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
