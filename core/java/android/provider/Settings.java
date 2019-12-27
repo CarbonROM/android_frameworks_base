cc8289ca31bc164e12a6bf9c42f6aa3a4ed8f405
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-12-27 12:34:35 +0100

    base: Hide pill in full gestural nav mode [1/2]
    - Originally extracted and decompiled from the HideNavBar magisk moudle.
    
    - Adjusted for CMake compilation and package names are changed as per
      our needs to dynamically load the overlays.
    
    source: https://github.com/Magisk-Modules-Repo/HideNavBar
    
    @SKULSHADY: Cleanup the overlays
    
    Change-Id: Idfb4943b54a6c6c674e141eda484377ad5e1c9ad
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4659,6 +4659,12 @@ public final class Settings {
         /** @hide */
         private static final Validator BACK_GESTURE_HEIGHT_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
+        /** @hide */
+        public static final String GESTURE_PILL_TOGGLE = "gesture_pill_toggle";
+
+        /** @hide */
+        private static final Validator GESTURE_PILL_TOGGLE_VALIDATOR = BOOLEAN_VALIDATOR;
+
         /**
          * Wheter to play notification sound and vibration if screen is ON
          * 0 - never
@@ -4733,6 +4739,7 @@ public final class Settings {
             ALARM_ALERT,
             NOTIFICATION_LIGHT_PULSE,
             BACK_GESTURE_HEIGHT,
+            GESTURE_PILL_TOGGLE,
         };
 
         /**
@@ -4857,6 +4864,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_TILE_TITLE_VISIBILITY);
             PRIVATE_SETTINGS.add(PROXIMITY_ON_WAKE);
             PRIVATE_SETTINGS.add(BACK_GESTURE_HEIGHT);
+            PRIVATE_SETTINGS.add(GESTURE_PILL_TOGGLE);
         }
 
         /**
@@ -4952,6 +4960,7 @@ public final class Settings {
             VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
             VALIDATORS.put(PROXIMITY_ON_WAKE, PROXIMITY_ON_WAKE_VALIDATOR);
             VALIDATORS.put(BACK_GESTURE_HEIGHT, BACK_GESTURE_HEIGHT_VALIDATOR);
+            VALIDATORS.put(GESTURE_PILL_TOGGLE, GESTURE_PILL_TOGGLE_VALIDATOR);
         }
 
         /**
