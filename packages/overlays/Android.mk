662a9e8e401ac38ad6b2f024bb7f0bb3c2dbc9ce
Pranav Vashi <neobuddy89@gmail.com>
2019-12-30 00:48:42 +0530

    Add hexagon and cylinder icon shape overlays
    Change-Id: Ie59442a991e33ff9b123ad3458e74b0f3df5bb24
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -69,8 +69,9 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedLauncherOverlay \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
-	IconPackRoundedThemePickerUIOverlay \
-	IconShapeCylinderOverlay \
+	IconPackRoundedThemePickerOverlay \
+  IconShapeCylinderOverlay \
+	IconShapeHexagonOverlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquareOverlay \
 	IconShapeSquircleOverlay \
