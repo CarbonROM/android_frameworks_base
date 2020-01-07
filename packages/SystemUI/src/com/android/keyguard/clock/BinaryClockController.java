4385b80e39e9a25436cebc0b5a869912808be84b
maxwen <max.weninger@gmail.com>
2020-01-07 21:50:30 +0300

    SystemUI: custom keyguard clock kick off
    Blissroms edit: Stripped down version of the original,
    remove one line and two line clock.
    
    AicpEdit: keep dateline
    
    allow custom clock to be in the same position as the default
    clock. Add bold hour clock back from the days where we
    had custom keyguard clock already
    
    Change-Id: I11bb2b0b70327bfc824b126f07043db49262c13b

--- packages/SystemUI/src/com/android/keyguard/clock/BinaryClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/BinaryClockController.java
@@ -55,11 +55,6 @@ public class BinaryClockController implements ClockPlugin {
     private final SysuiColorExtractor mColorExtractor;
 
     /**
-     * Computes preferred position of clock.
-     */
-    private final SmallClockPosition mClockPosition;
-
-    /**
      * Renders preview from clock view.
      */
     private final ViewPreviewer mRenderer = new ViewPreviewer();
@@ -69,12 +64,6 @@ public class BinaryClockController implements ClockPlugin {
      */
     private BinaryClock mBinaryClock;
     private ClockLayout mBigClockView;
-
-    /**
-     * Small clock shown on lock screen above stack scroller.
-     */
-    private View mView;
-    private TextClock mLockClock;
 
     /**
      * Helper to extract colors from wallpaper palette for clock face.
@@ -93,23 +82,17 @@ public class BinaryClockController implements ClockPlugin {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
-        mClockPosition = new SmallClockPosition(res);
     }
 
     private void createViews() {
         mBigClockView = (ClockLayout) mLayoutInflater.inflate(R.layout.binary_clock, null);
-        mBinaryClock = mBigClockView.findViewById(R.id.analog_clock);
-
-        mView = mLayoutInflater.inflate(R.layout.digital_clock, null);
-        mLockClock = mView.findViewById(R.id.lock_screen_clock);
+        mBinaryClock = mBigClockView.findViewById(R.id.binary_clock);
     }
 
     @Override
     public void onDestroyView() {
         mBigClockView = null;
         mBinaryClock = null;
-        mView = null;
-        mLockClock = null;
     }
 
     @Override
@@ -145,10 +128,7 @@ public class BinaryClockController implements ClockPlugin {
 
     @Override
     public View getView() {
-        if (mView == null) {
-            createViews();
-        }
-        return mView;
+        return null;
     }
 
     @Override
@@ -161,11 +141,8 @@ public class BinaryClockController implements ClockPlugin {
 
     @Override
     public int getPreferredY(int totalHeight) {
-        return mClockPosition.getPreferredY();
+        return totalHeight / 2;
     }
-
-    @Override
-    public void setStyle(Style style) {}
 
     @Override
     public void setTextColor(int color) {
@@ -189,13 +166,11 @@ public class BinaryClockController implements ClockPlugin {
     public void onTimeTick() {
         mBinaryClock.onTimeChanged();
         mBigClockView.onTimeChanged();
-        mLockClock.refresh();
     }
 
     @Override
     public void setDarkAmount(float darkAmount) {
         mPalette.setDarkAmount(darkAmount);
-        mClockPosition.setDarkAmount(darkAmount);
         mBigClockView.setDarkAmount(darkAmount);
         boolean dark = darkAmount == 1;
         mBinaryClock.setDark(dark);
