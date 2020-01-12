272dc1fcac1f4f1f2c7bc1640e7f221e5ba20759
ezio84 <brabus84@gmail.com>
2020-01-12 18:53:46 +0100

    Flashlight toggle API: extend to screenOn state for Elmyra actions
    Change-Id: Id25d1bbb81caee0c9c362c917df269d78d064340

--- packages/SystemUI/src/com/android/systemui/doze/DozeHost.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeHost.java
@@ -87,11 +87,13 @@ public interface DozeHost {
          * @param active whether power save is active or not
          */
         default void onPowerSaveChanged(boolean active) {}
-        default void toggleCameraFlash() {}
+        default void toggleFlashlightProximityCheck() {}
     }
 
     interface PulseCallback {
         void onPulseStarted();
         void onPulseFinished();
     }
+
+    void performToggleFlashlight();
 }
