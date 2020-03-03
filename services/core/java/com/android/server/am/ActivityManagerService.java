24d3011a22c32d94e1c1aef402ef6778166f0b23
Riddle Hsu <riddlehsu@google.com>
2020-03-03 14:36:21 +0800

    RESTRICT AUTOMERGE Use consistent calling uid and package in navigateUpTo
    Originally, if the caller of navigateUpTo is alive, even the calling
    uid is set to the caller who launched the existing destination activity,
    the uid from caller process has higher priority to replace the given
    calling uid. So this change doesn't modify the existing behavior if
    the caller process is valid. Besides, the case of delivering new intent
    uses the source record as calling identity too, so the case of starting
    new activity should be consistent.
    
    Also forbid attaching null application thread to avoid unexpected state
    in process record.
    
    Bug: 144285917
    Test: bit FrameworksServicesTests:com.android.server.am.ActivityStackTests
    Change-Id: I60732f430256d37cb926d08d093581f051c4afed
    (cherry picked from commit 0d7e27af30e39fbb6dcafedc854daa639074e5cc)

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -6941,7 +6941,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
     }
 
-    private final boolean attachApplicationLocked(IApplicationThread thread,
+    private boolean attachApplicationLocked(@NonNull IApplicationThread thread,
             int pid) {
 
         // Find the application record that is being attached...  either via
@@ -7246,6 +7246,9 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     @Override
     public final void attachApplication(IApplicationThread thread) {
+        if (thread == null) {
+            throw new SecurityException("Invalid application interface");
+        }
         synchronized (this) {
             int callingPid = Binder.getCallingPid();
             final long origId = Binder.clearCallingIdentity();
