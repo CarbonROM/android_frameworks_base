ezio84/abc-n_frameworks_base

Power flashlight: allow to use it when all ambient/wake options are off
now that we linked the proximity sensor check with aosp ambient code,
we need to enable doze services also when the user disables all ambient
and wake options

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -55,7 +55,8 @@ public class AmbientDisplayConfiguration {
                 || pickupGestureEnabled(user)
                 || tapGestureEnabled(user)
                 || doubleTapGestureEnabled(user)
-                || isAmbientTickerEnabled(user);
+                || isAmbientTickerEnabled(user)
+                || isPowerBtnFlashlightEnabled(user);
     }
 
     /** {@hide} */
@@ -247,5 +248,11 @@ public class AmbientDisplayConfiguration {
     /** {@hide} */
     public boolean deviceHasSoli() {
         return mDeviceHasSoli;
+    }
+
+    /** {@hide} */
+    public boolean isPowerBtnFlashlightEnabled(int user) {
+        return Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0, user) != 0;
     }
 }

CarbonROM/android_frameworks_base

Change-Id: I25f9f821bbde5bf3d87a1f3d3b7fc26d9aea9a92

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -52,7 +52,8 @@ public class AmbientDisplayConfiguration {
                 || wakeDisplayGestureEnabled(user)
                 || pickupGestureEnabled(user)
                 || tapGestureEnabled(user)
-                || doubleTapGestureEnabled(user);
+                || doubleTapGestureEnabled(user)
+                || isPowerBtnFlashlightEnabled(user);
     }
 
     /** {@hide} */
@@ -212,5 +213,11 @@ public class AmbientDisplayConfiguration {
 
     private boolean boolSetting(String name, int user, int def) {
         return Settings.Secure.getIntForUser(mContext.getContentResolver(), name, def, user) != 0;
+    }
+
+    /** {@hide} */
+    public boolean isPowerBtnFlashlightEnabled(int user) {
+        return Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0, user) != 0;
     }
 }