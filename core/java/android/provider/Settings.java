ca2361b6f461a628442c60d428768ed424a5fd9f
Max Weffers <rcstar6696@gmail.com>
2020-03-26 18:32:10 +0100

    Configure lockscreen media artwork blur level
    Change-Id: I68895ae6a471bbc99b538b85337e15e8793665ce

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4855,7 +4855,17 @@ public final class Settings {
         * @hide
         */
         public static final String SWAP_VOLUME_KEYS_ON_ROTATION = "swap_volume_keys_on_rotation";
-        
+
+        /**
+        * Media artwork wallpaper blur level on lockscreen
+        * @hide
+        */
+       public static final String LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
+
+       /** @hide */
+       private static final Validator LOCKSCREEN_MEDIA_BLUR_VALIDATOR =
+               NON_NEGATIVE_INTEGER_VALIDATOR;
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
@@ -5058,6 +5068,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(LOCK_SHOW_STATUS_BAR);
             PRIVATE_SETTINGS.add(ENABLE_CONDITIONS);
             PRIVATE_SETTINGS.add(ENABLE_SUGGESTIONS);
+            PRIVATE_SETTINGS.add(LOCKSCREEN_MEDIA_BLUR);
         }
 
         /**
@@ -5159,6 +5170,7 @@ public final class Settings {
             VALIDATORS.put(LOCK_SHOW_STATUS_BAR,LOCK_SHOW_STATUS_BAR_VALIDATOR);
             VALIDATORS.put(ENABLE_CONDITIONS, ENABLE_CONDITIONS_VALIDATOR);
             VALIDATORS.put(ENABLE_SUGGESTIONS, ENABLE_SUGGESTIONS_VALIDATOR);
+            VALIDATORS.put(LOCKSCREEN_MEDIA_BLUR, LOCKSCREEN_MEDIA_BLUR_VALIDATOR);
         }
 
         /**
