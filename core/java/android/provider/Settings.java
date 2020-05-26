872bea849fcaf4db919e75751837f223d2065818
althafvly <althafvly@gmail.com>
2020-05-26 21:17:59 +0800

    SystemUI: support black theme for dark mode [1/4]
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Change-Id: I57cd53de8f2c1e4d445441b514875b6af915b858

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11213,6 +11213,12 @@ public final class Settings {
         public static final String LOCK_SCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
 
         /**
+         * Whether to enable our custom black theme
+         * @hide
+         */
+        public static final String BERRY_BLACK_THEME = "berry_black_theme";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
@@ -11313,7 +11319,7 @@ public final class Settings {
          * @hide
          */
         public static final String[] CARBON_SECURE_SETTINGS = {
-            // FOO_BAR_BALABALA
+            BERRY_BLACK_THEME
         };
 
         /**
@@ -11327,7 +11333,7 @@ public final class Settings {
          */
         public static final Map<String, Integer> CARBON_SECURE_SETTINGS_VALIDATORS = new ArrayMap<>();
         static {
-            //CARBON_SECURE_SETTINGS_VALIDATORS.put(FOO_BAR_BALABALA, 1);
+            CARBON_SECURE_SETTINGS_VALIDATORS.put(BERRY_BLACK_THEME, 0);
         }
     }
 
