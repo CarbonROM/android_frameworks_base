aedabc6c38ef2ae36e95c1af221c37d3d18cbce9
ezio84 <brabus84@gmail.com>
2020-05-30 15:27:04 +0200

    Battery icon: fix requestLayout() calls spam when in lockscreen
    if percentage text is set outside the icon
    
    KeyguardStatusBarView.updateVisibilities calls setForceShowPercent,
    ending up calling updatePercentText
    updatePercentText method gets called in many cases in BatteryMeterView
    Actually i'm really busy so as a quick fix let's just skip all useless
    mBatteryPercentView.setText calls
    
    Change-Id: I2e559cf80b47ee2829fda3437f55bb16e845b277

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -349,7 +349,7 @@ public class BatteryMeterView extends LinearLayout implements
                 mBatteryController.getEstimatedTimeRemainingString((String estimate) -> {
                     if (estimate != null) {
                         if (mBatteryPercentView != null) {
-                            mBatteryPercentView.setText(estimate);
+                            batteryPercentViewSetText(estimate);
                         }
                         setContentDescription(getContext().getString(
                                 R.string.accessibility_battery_level_with_estimate,
@@ -375,7 +375,7 @@ public class BatteryMeterView extends LinearLayout implements
             String bolt = "\u26A1\uFE0E";
             CharSequence mChargeIndicator = mCharging && (mBatteryStyle == BATTERY_STYLE_HIDDEN)
                 ? (bolt + " ") : "";
-            mBatteryPercentView.setText(mChargeIndicator +
+            batteryPercentViewSetText(mChargeIndicator +
                 NumberFormat.getPercentInstance().format(mLevel / 100f));
             setContentDescription(
                     getContext().getString(mCharging ? R.string.accessibility_battery_level_charging
@@ -437,6 +437,13 @@ public class BatteryMeterView extends LinearLayout implements
             mBatteryIconView.setVisibility(View.VISIBLE);
             //setVisibility(View.VISIBLE);
             scaleBatteryMeterViews();
+        }
+    }
+
+    private void batteryPercentViewSetText(CharSequence text) {
+        CharSequence currentText = mBatteryPercentView.getText();
+        if (!currentText.toString().equals(text.toString())) {
+            mBatteryPercentView.setText(text);
         }
     }
 
