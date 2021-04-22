6e95768c286b43392ae246bbaaef3c042ced63c2
HrX03 <dn.bianco03@gmail.com>
2021-04-22 19:15:32 +0200

    VolumeDialogImpl: Remove insets listener on dismiss
    Signed-off-by: Arian <arian.kulmer@web.de>
    Change-Id: I880bfcedad40f2cd4f602709f2ba963aad5ac4cb

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1031,6 +1031,7 @@ public class VolumeDialogImpl implements VolumeDialog,
                 }, 50));
         if (!isLandscape() || !mShowActiveStreamOnly) animator.translationX(getAnimatorX());
         animator.start();
+        mDialog.getViewTreeObserver().removeOnComputeInternalInsetsListener(mInsetsListener);
         checkODICaptionsTooltip(true);
         synchronized (mSafetyWarningLock) {
             if (mSafetyWarning != null) {
