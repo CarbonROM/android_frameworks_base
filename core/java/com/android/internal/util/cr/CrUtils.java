84d0f4e4b9c68863febdd4abac5c45462f40e82f
Jacob McSwain <jacob@mcswain.dev>
2021-10-17 17:17:54 -0500

    Revert "CRUtils: Add isWiFi only Method"
    This reverts commit 2ff71f8a4a4d5d41394f2b6ddaeadaefedd7bb38.
    
    Nothing currently uses this, and these APIs have been deprecated
    
    Change-Id: Ida8fade59dc6c10a17f93ed5384a8d30c9c4c918

--- core/java/com/android/internal/util/cr/CrUtils.java
+++ core/java/com/android/internal/util/cr/CrUtils.java
@@ -24,7 +24,6 @@ import android.content.res.Resources;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
-import android.net.ConnectivityManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.util.DisplayMetrics;
@@ -102,12 +101,6 @@ public class CrUtils {
             // Ignore
         }
         return false;
-    }
-
-    public static boolean isWifiOnly(Context context) {
-        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(
-                Context.CONNECTIVITY_SERVICE);
-        return (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE) == false);
     }
 
     public static boolean deviceHasFlashlight(Context ctx) {
