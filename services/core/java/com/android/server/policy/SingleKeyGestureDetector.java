0818e5ee600df0064e216e3edcd16e923e51263b
ezio84 <brabus84@gmail.com>
2021-10-21 18:07:40 +0000

    Allow doubletap/longpress power to toggle torch [1/2]
    CarbonROM A13 Edits:
    - Properly implement Double- and Longpress based on existing AOSP Frameworks
    - Use Dozes Proximity check to prevent accidentally turning on the flashlight
    - Increase DP Delay to 200ms. This still decreases the duration as we previously had a lot more overhead between the doublepress check.
    
    NB: for further flashlight action implementations (e.g. squeeze for
    flashlight) when screen is ON, just call toggleCameraFlash(),
    the following statusbar code is already dynamic and able to
    check proximity if screen is off, and to toggle flashlight straight
    if screen is ON
    
    Change-Id: If450880280d46993826d5636c71b0427e7d75a3e
    ---
    
    Thanks beanstown106 and lineage guys for the initial longpress action
    bringup in PhoneWindowManager back in the days
    
    If the user enables double tap for camera action, torch action will
    automatically switch to long press mode.
    If the user enables double tap for torch, double tap for camera
    will be disabled accordingly.
    
    Remove the settings pref is the device doesn't have flashlight
    
    A 170ms delay for double tap action is not annoying on single tap
    to wake up the device.
    
    Vibrate when toggling torch to let the user know of unwanted toggling
    in the pocket (even if vibration is disabled by user)
    
    Allow torch action also on ambient display
    
    Change-Id: I12da044f86c7b625872607529cf8524615cf576b
    
    Power button flashlight toggle: add proximity sensor check
    
    cm/lineage guys managed the proximity sensor in a really nice and reliable
    way for the proximity wake feature, so let's kang and adapt it to block
    flashlight toggling when the sensor is covered.
    
    Change-Id: I0851df54341c4223e90a8926ca762c408c4a2b89
    
    Power button flashlight: allow to skip proximity check per device
    
    set config_flashlightProximityTimeout to -1
    
    some devices have an ultrasound proximity sensor that seems unable
    to work when screen is off
    
    Change-Id: I82cc1cbf123823987e7ae45e52c29a37d4d3936d
    
    Power button flashlight toggle: rely on aosp doze sensors check code
    
    Till now we were checking proximity sensor before toggling the
    flashlight with our custom implementation.
    For doze pulsing, Google have a solid code implementation to check the
    same, and since some months they even made it better using a
    prox gated brightness sensor if available (e.g. Pixel phones).
    Let's just use it.
    
    Power button flashlight toggle: rely on aosp doze sensors check code
    
    Till now we were checking proximity sensor before toggling the
    flashlight with our custom implementation.
    For doze pulsing, Google have a solid code implementation to check the
    same, and since some months they even made it better using a
    prox gated brightness sensor if available (e.g. Pixel phones).
    Let's just use it.
    
    Flashlight toggle API: extend to screenOn state
    
    Change-Id: Id25d1bbb81caee0c9c362c917df269d78d064340
    
    Power flashlight: allow to use it when all ambient/wake options are off
    
    now that we linked the proximity sensor check with aosp ambient code,
    we need to enable doze services also when the user disables all ambient
    and wake options
    
    Change-Id: If18ddac6d268db133d8a64356dd0694ecc1087df

--- services/core/java/com/android/server/policy/SingleKeyGestureDetector.java
+++ services/core/java/com/android/server/policy/SingleKeyGestureDetector.java
@@ -127,6 +127,13 @@ public final class SingleKeyGestureDetector {
         }
 
         /**
+         *  Return max Key Timeout for gesture detection
+         */
+        long getMultiPressTimeout() {
+            return MULTI_PRESS_TIMEOUT;
+        }
+
+        /**
          *  Called when short press has been detected.
          */
         abstract void onPress(long downTime);
@@ -250,7 +257,7 @@ public final class SingleKeyGestureDetector {
 
         final long keyDownInterval = event.getDownTime() - mLastDownTime;
         mLastDownTime = event.getDownTime();
-        if (keyDownInterval >= MULTI_PRESS_TIMEOUT) {
+        if (keyDownInterval >= mActiveRule.getMultiPressTimeout()) {
             mKeyPressCounter = 1;
         } else {
             mKeyPressCounter++;
@@ -324,7 +331,7 @@ public final class SingleKeyGestureDetector {
                 Message msg = mHandler.obtainMessage(MSG_KEY_DELAYED_PRESS, mActiveRule.mKeyCode,
                         mKeyPressCounter, mActiveRule);
                 msg.setAsynchronous(true);
-                mHandler.sendMessageDelayed(msg, MULTI_PRESS_TIMEOUT);
+                mHandler.sendMessageDelayed(msg, mActiveRule.getMultiPressTimeout());
             }
             return true;
         }
