omnirom/android_frameworks_base_old

SystemUI: init icon blacklist in settings
we cant access config_statusBarIconBlackList outside so make
sure its values is written as default

Change-Id: Ic9e8d140f399346b25866811bd0e333bf8d881dc

packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -97,6 +97,8 @@ public class TunerServiceImpl extends TunerService {
             }
         }
 
+        initIconBlacklist();
+
         mCurrentUser = ActivityManager.getCurrentUser();
         mUserTracker = new CurrentUserTracker(broadcastDispatcher) {
             @Override
@@ -114,21 +116,18 @@ public class TunerServiceImpl extends TunerService {
         mUserTracker.stopTracking();
     }
 
-    private void upgradeTuner(int oldVersion, int newVersion, Handler mainHandler) {
-        if (oldVersion < 1) {
-            String blacklistStr = getValue(StatusBarIconController.ICON_BLACKLIST);
-            if (blacklistStr != null) {
-                ArraySet<String> iconBlacklist =
-                        StatusBarIconController.getIconBlacklist(mContext, blacklistStr);
-
-                iconBlacklist.add("rotate");
-                iconBlacklist.add("headset");
-
-                Settings.Secure.putStringForUser(mContentResolver,
-                        StatusBarIconController.ICON_BLACKLIST,
-                        TextUtils.join(",", iconBlacklist), mCurrentUser);
-            }
+    private void initIconBlacklist() {
+        String blacklistStr = getValue(StatusBarIconController.ICON_BLACKLIST);
+        if (blacklistStr == null) {
+            ArraySet<String> iconBlacklist =
+                    StatusBarIconController.getIconBlacklist(mContext, blacklistStr);
+            Settings.Secure.putStringForUser(mContentResolver,
+                    StatusBarIconController.ICON_BLACKLIST,
+                    TextUtils.join(",", iconBlacklist), mCurrentUser);
         }
+    }
+
+    private void upgradeTuner(int oldVersion, int newVersion, Handler mainHandler) {
         if (oldVersion < 2) {
             setTunerEnabled(mContext, false);
         }

CarbonROM/android_frameworks_base

packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -116,6 +116,8 @@ public class TunerServiceImpl extends TunerService {
             }
         }
 
+        initIconHideList();
+
         mCurrentUser = mUserTracker.getUserId();
         mCurrentUserTracker = new UserTracker.Callback() {
             @Override
@@ -134,21 +136,18 @@ public class TunerServiceImpl extends TunerService {
         mUserTracker.removeCallback(mCurrentUserTracker);
     }
 
-    private void upgradeTuner(int oldVersion, int newVersion, Handler mainHandler) {
-        if (oldVersion < 1) {
-            String hideListStr = getValue(StatusBarIconController.ICON_HIDE_LIST);
-            if (hideListStr != null) {
-                ArraySet<String> iconHideList =
-                        StatusBarIconController.getIconHideList(mContext, hideListStr);
-
-                iconHideList.add("rotate");
-                iconHideList.add("headset");
-
-                Settings.Secure.putStringForUser(mContentResolver,
-                        StatusBarIconController.ICON_HIDE_LIST,
-                        TextUtils.join(",", iconHideList), mCurrentUser);
-            }
+    private void initIconHideList() {
+        String hideListStr = getValue(StatusBarIconController.ICON_HIDE_LIST);
+        if (hideListStr == null) {
+            ArraySet<String> iconHideList =
+                    StatusBarIconController.getIconHideList(mContext, hideListStr);
+            Settings.Secure.putStringForUser(mContentResolver,
+                    StatusBarIconController.ICON_HIDE_LIST,
+                    TextUtils.join(",", iconHideList), mCurrentUser);
         }
+    }
+
+    private void upgradeTuner(int oldVersion, int newVersion, Handler mainHandler) {
         // 3 Removed because of a revert.
         if (oldVersion < 4) {
             // Delay this so that we can wait for everything to be registered first.