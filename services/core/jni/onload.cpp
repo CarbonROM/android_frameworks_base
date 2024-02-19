242c822a91a6e8b976350f0e2117029d02ef3b76
LuK1337 <priv.luk@gmail.com>
2024-02-19 16:20:04 +0100

    Allow signature spoofing for microG Companion/Services
    This patch enables signature spoofing when the following conditions are
    met:
    * Build is debuggable (userdebug/eng)
    * Package name is com.android.vending or com.google.android.gms
    * Package is signed with microG release keys
    * Fake signature is correct
    
    Additionally, we let these apps be forceQueryable if they so desire.
    
    Change-Id: I8fc82ed266a2cc59636b662c7ea7e29c94f509b5

--- services/core/jni/onload.cpp
+++ services/core/jni/onload.cpp
@@ -57,6 +57,7 @@ int register_android_server_net_NetworkStatsService(JNIEnv* env);
 int register_android_server_security_VerityUtils(JNIEnv* env);
 int register_android_server_am_AppCompactor(JNIEnv* env);
 int register_android_server_am_LowMemDetector(JNIEnv* env);
+int register_android_server_com_android_server_pm_PackageManagerService(JNIEnv* env);
 };
 
 using namespace android;
@@ -107,5 +108,6 @@ extern "C" jint JNI_OnLoad(JavaVM* vm, void* /* reserved */)
     register_android_server_security_VerityUtils(env);
     register_android_server_am_AppCompactor(env);
     register_android_server_am_LowMemDetector(env);
+    register_android_server_com_android_server_pm_PackageManagerService(env);
     return JNI_VERSION_1_4;
 }
